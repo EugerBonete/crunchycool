09e874ab8f2da38c17b8678fe031c889833a5d7a
euger <azkriven17@gmail.com>
2023-10-06 23:48:17 +0800

    added search functionality server 1

--- app/(dashboard)/search/page.tsx
+++ app/(dashboard)/search/page.tsx
@@ -1,25 +1,42 @@
 "use client";
+import { useSearch } from "@/context/use-search";
+import { IAnimeResult } from "@/types";
 import { Image, Input, Radio, RadioGroup } from "@nextui-org/react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Search() {
-  const [selected, setSelected] = useState("gogoanime");
+  const [server, setServer] = useState("gogoanime");
   const [query, setQuery] = useState("");
-  console.log(query);
 
+  const { data, isLoading, isFetching, isError, refetch } = useSearch({
+    id: query,
+    server: server,
+    type: "anime",
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    refetch();
+  };
+
+  console.log(data);
   return (
-    <div className="min-h-[300px] px-5 md:px-10 max-w-5xl mx-auto sticky left-0 top-0">
+    <form
+      onSubmit={handleSubmit}
+      className="min-h-[300px] px-5 md:px-10 max-w-5xl mx-auto sticky left-0 top-0"
+    >
       <RadioGroup
         label="What do you want to search?"
         className="my-5"
-        value={selected}
-        onValueChange={setSelected}
+        value={server}
+        onValueChange={setServer}
       >
         <Radio value="gogoanime">Anime Server 1</Radio>
         <Radio value="anilist">Anime Server 2</Radio>
         <Radio value="manga">Manga</Radio>
         <Radio value="movies">Movies</Radio>
       </RadioGroup>
+
       <Input
         radius="none"
         size="lg"
@@ -32,6 +49,16 @@ export default function Search() {
         }}
         required
       />
-    </div>
+      {data?.results?.length && (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.results.map((data: IAnimeResult) => (
+            <div key={data.id}>
+              <img src={data.image} alt="" />
+            </div>
+          ))}
+        </div>
+      )}
+      {isFetching && "..."}
+    </form>
   );
 }
